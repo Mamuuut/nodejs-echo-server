9f1ff0107242d6dc3714543217e551941fa4a96e
Mathieu Delaunay <mathieu.delaunay@dezem.de>
2018-07-02 10:15:10 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// https://gist.github.com/tedmiston/5935757
+// https://nodejs.org/api/stream.html#stream_readable_pipe_destination_options
+
+var net = require('net');
+
+var iNbConnections = 0;
+
+var server = net.createServer(function(socket)
+{
+    console.log(++iNbConnections, 'connected client(s)');
+    socket.write('Echo server\r\n');
+    socket.pipe(socket);
+
+    socket.on('close', function()
+    {
+        console.log(--iNbConnections, 'connected client(s)');
+    });
+
+});
+
+console.log('Listening on 127.0.0.1:1337');
+server.listen(1337, '127.0.0.1');
